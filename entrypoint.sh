6ecbd59db43c25a2d63b12778bdfb35c8c6f1197
bz.yang <48201330+bzKKday@users.noreply.github.com>
2022-05-05 15:17:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -65,7 +65,7 @@ users:
 - name: eks_$cluster
   user:
     exec:
-      apiVersion: client.authentication.k8s.io/v1alpha1
+      apiVersion: client.authentication.k8s.io/v1beta1
       command: aws-iam-authenticator
       args:
         - "token"
